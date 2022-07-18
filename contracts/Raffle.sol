a956f49b508cccd1470862a23a806ec57da7cd0e
Atiqur Rahman <atiqur.india@gmail.com>
2022-07-18 16:53:51 +0000

    Import VRFComsumerBaseV2

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -1,30 +1,42 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+
 error Raffle__NotEnoughETHEntered();
 
-contract Raffle {
-    /* State Variables */ 
+contract Raffle is VRFConsumerBaseV2 {
+    /* State Variables */
     uint256 private immutable i_entranceFee;
     address payable[] private s_players;
 
     /* Events */
     event RaffleEnter(address indexed player);
 
-    constructor(uint256 entranceFee) {
+    constructor(address vrfCoordinatorV2, uint256 entranceFee) VRFConsumerBaseV2(vrfCoordinatorV2) {
         i_entranceFee = entranceFee;
     }
 
     function enterRaffle() public payable {
-        if(msg.value < i_entranceFee) {
+        if (msg.value < i_entranceFee) {
             revert Raffle__NotEnoughETHEntered();
         }
         s_players.push(payable(msg.sender));
         // Emit an event when we update a dynamic array or mapping
         emit RaffleEnter(msg.sender);
-        
     }
 
+    function requestRandomWinner() external {
+        // Request the random number
+        // Once we git it, do something with it
+    }
+
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords)
+        internal
+        override
+    {}
+
+    // View / Pure functions
     function getEntranceFee() public view returns (uint256) {
         return i_entranceFee;
     }
